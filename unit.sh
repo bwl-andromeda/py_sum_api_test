88f572666684b7cf79c2d19dfbd4ece254bda30c
torchhik <torchhik_darkness@mail.ru>
2024-08-21 15:05:06 +0300

    new

--- unit.sh
+++ unit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/torchhik/rep/testovoe/py_sum_api_test 
+cd /home/torchhik/rep/py_sum_api_test 
 source .venv/bin/activate
-exec uvicorn main:app --host 0.0.0.0 --port 8000
+exec uvicorn main:app --host 0.0.0.0 --port 8000 
 
